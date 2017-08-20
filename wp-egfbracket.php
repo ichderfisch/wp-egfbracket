4fbc91c689294ae922df067839b7d965dc3eb167
ichderfisch <me@ichderfisch.de>
2017-08-20 16:06:45 +0200

    added medals to css + minor security fix

--- wp-egfbracket.php
+++ wp-egfbracket.php
@@ -3,10 +3,13 @@
   Plugin Name: EGF Bracket
   Plugin URI: https://github.com/ichderfisch/wp-egfbracket
   Description: Plugin that adds a shortcode to display tournament brackets by the European Go Federation visible
-  Version: 0.0.1
+  Version: 0.0.2
   Author: Dennis Fischer
   Author URI: http://www.ichderfisch.de
 */
+
+// Prevent direct file access.
+if (!defined ('ABSPATH')) exit;
 
 /**
  * Add CSS & JS
@@ -14,14 +17,14 @@
 function register_egfbracket_css_js() {
   
   // Register CSS
-	wp_register_style( 'egfbracket-css', plugins_url( 'wp-egfbracket/css/style.css' ) );
+  wp_register_style( 'egfbracket-css', plugins_url( 'wp-egfbracket/css/style.css' ) );
   
   // Register JS
   wp_register_script( 'egfbracket-raphael', plugins_url( 'wp-egfbracket/js/raphael.js' ) );
   wp_register_script( 'egfbracket-treant', plugins_url( 'wp-egfbracket/js/Treant_Round_Titles.js' ) );  
   
   // Enqueue them all
-	wp_enqueue_style( 'egfbracket-css' );
+  wp_enqueue_style( 'egfbracket-css' );
   wp_enqueue_script( 'egfbracket-raphael' );
   wp_enqueue_script( 'egfbracket-treant' );
 }
@@ -33,23 +36,23 @@ add_action( 'wp_enqueue_scripts', 'register_egfbracket_css_js' );
 // Add Shortcode
 function show_egfbrackets_results( $atts ) {
 
-	// Attributes
-	$atts = shortcode_atts(array(
-			'id' => '1',
-		), $atts
-	);
+  // Attributes
+  $atts = shortcode_atts(array(
+      'id' => '1',
+    ), $atts
+  );
   
   $bracketIdCssUrl = 'http://www.eurogofed.org/newick/tournament.css?id=' . $atts['id'];
   $bracketIdJsUrl = 'http://www.eurogofed.org/newick/script.js?id=' . $atts['id'];
-	
-	$output = ' 
+  
+  $output = ' 
               <link rel="stylesheet" href="'. $bracketIdCssUrl . '">
-							<script src="' . $bracketIdJsUrl . '"></script>
+              <script src="' . $bracketIdJsUrl . '"></script>
               
-							<div class="chart' . $atts['id'] . ' alignCenter" id="tournament' . $atts['id']. '"></div> 
-							<script> new Treant(tournament' . $atts['id'] .'); </script>';
-	
-	return $output;
+              <div class="chart' . $atts['id'] . ' alignCenter" id="tournament' . $atts['id']. '"></div> 
+              <script> new Treant(tournament' . $atts['id'] .'); </script>';
+  
+  return $output;
 
 }
 
